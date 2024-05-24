6c015d8be7aaccada0a1b4fdb898438a94a9acda
Gabriel <mgabriel.moliveira@gmail.com>
2024-05-24 19:42:44 -0300

    configuring the website deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // <=== enables static exports
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 export default nextConfig;
