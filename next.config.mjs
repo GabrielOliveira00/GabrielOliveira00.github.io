88b846ce55cdb6d0e83948a4976960aba11bfc03
Gabriel <mgabriel.moliveira@gmail.com>
2024-05-24 19:38:46 -0300

    configuring the website deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/training-api",
   output: "export", // <=== enables static exports
-  reactStrictMode: true,
 };
 export default nextConfig;
